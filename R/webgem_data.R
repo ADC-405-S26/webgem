a929610c8a3da732890d100abcad1cf4695fb4e9
connerrob24 <robertco@lawrence.edu>
2026-05-26 23:59:38 +0000

    added readme and toy dataset (webgem_data) for the package.

---
+++ R/webgem_data.R
@@ -0,0 +1,21 @@
+#' A toy dataset for demonstrating the functions in webgem
+#'
+#'
+#'
+#' @format ## `webgem_data`
+#' A data frame with 25 rows and 12 columns:
+#' \describe{
+#'   \item{player}{The name of the player being analyzed}
+#'   \item{wrc_plus}{Weighted runs created plus}
+#'   \item{ops}{Test score of each subject}
+#'   \item{obp}{Words recalled by each subject}
+#'   \item{slg}{Slugging percentage}
+#'   \item{avg}{Batting average}
+#'   \item{iso}{Isolated power}
+#'   \item{war}{Wins above replacement}
+#'   \item{woba}{Weighted on-base average}
+#'   \item{bb_pct}{Walk percentage}
+#'   \item{barrel_pct}{Barrel percentage}
+#'   \item{hard_hit_pct}{Hard-hit percentage}
+#' }
+"webgem_data"
